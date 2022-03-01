f542af0c55e3e1558d54decbc36f680ea7586271
bayupw <bayupw@gmail.com>
2022-03-02 11:55:34 +1300

    first commit

---
+++ main.tf
@@ -0,0 +1,51 @@
+# ---------------------------------------------------------------------------------------------------------------------
+# AWS Transit
+# ---------------------------------------------------------------------------------------------------------------------
+module "aws_transit_1" {
+  source                = "terraform-aviatrix-modules/mc-transit/aviatrix"
+  cloud                 = "aws"
+  name                  = "transit"
+  region                = var.aws_region
+  cidr                  = cidrsubnet(var.cloud_supernet, 8, 255)
+  account               = var.aws_account
+  insane_mode           = var.hpe
+  instance_size         = var.aws_instance_size
+  ha_gw                 = var.ha_gw
+  local_as_number       = 65501
+  learned_cidr_approval = true
+}
+
+# ---------------------------------------------------------------------------------------------------------------------
+# AWS Spoke
+# ---------------------------------------------------------------------------------------------------------------------
+module "aws_spoke_1" {
+  source        = "terraform-aviatrix-modules/mc-spoke/aviatrix"
+  cloud         = "AWS"
+  name          = "spoke1"
+  cidr          = cidrsubnet(var.cloud_supernet, 8, 100)
+  region        = var.aws_region
+  account       = var.aws_account
+  transit_gw    = module.aws_transit_1.transit_gateway.gw_name
+  insane_mode   = var.hpe
+  instance_size = var.aws_instance_size
+  ha_gw         = var.ha_gw
+}
+
+# ---------------------------------------------------------------------------------------------------------------------
+# On-Prem
+# ---------------------------------------------------------------------------------------------------------------------
+module "onprem" {
+  source              = "github.com/gleyfer/aviatrix-demo-onprem-aws"
+  hostname            = "onprem"
+  tunnel_proto        = "IPsec"
+  network_cidr        = cidrsubnet(var.onprem_supernet, 8, 200)
+  public_subnets      = [cidrsubnet(var.onprem_supernet, 9, 400)]
+  private_subnets     = [cidrsubnet(var.onprem_supernet, 9, 401)]
+  instance_type       = "t3.medium"
+  public_conns        = ["${module.aws_transit_1.transit_gateway.gw_name}:${module.aws_transit_1.transit_gateway.local_as_number}:1"]
+  csr_bgp_as_num      = "65511"
+  create_client       = false
+  advertised_prefixes = ["0.0.0.0/0"]
+
+  depends_on = [module.aws_transit_1]
+}
